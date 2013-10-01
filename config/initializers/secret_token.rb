b488dbcb98a6594cc9abd7e5610124cd4d389a39
Jon Girard <jongirard03@gmail.com>
2013-09-30 21:58:43 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PlentusPreLaunch::Application.config.secret_key_base = '0fef23e765375a0591ef4e6040cb08b124994efc535e2e8f564baab13583bb1fa469ac058731f9f5b25042c6d07fb5589f1a1c57b16bc501679a879138f06386'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PlentusPreLaunch::Application.config.session_store :cookie_store, key: '_plentus-pre-launch_session'
