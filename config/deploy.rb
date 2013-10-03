1d8e2064b9a9159b5a3e0f19d48155d6e8537127
Jon Girard <jongirard03@gmail.com>
2013-10-03 01:51:32 -0700

    fix broken git link

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "git@github.com:jongirard/#{application}.git"
+set :repository, "https://github.com/jongirard/#{application}.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
