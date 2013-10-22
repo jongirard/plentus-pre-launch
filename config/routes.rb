2beaac21344b094d989142d26bbf16149300b405
Jon Girard <jongirard03@gmail.com>
2013-10-21 18:10:06 -0700

    users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 PlentusPreLaunch::Application.routes.draw do
   resources :visitors, only: [:new, :create]
+  resources :users, only: [:new, :create]
   root to: 'visitors#new'
   match '*path' => redirect('/'), via: :get
 end
