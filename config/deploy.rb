703c56c6466cbba9c4e7419429cbfb92a3182cdc
Jon Girard <jongirard03@gmail.com>
2013-10-29 13:46:49 -0700

    deploy.rb updates

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,8 +23,8 @@ set :branch, "master"
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 ssh_options[:port] = 222
+set :rails_env, "production"
 
-set :rails_env, 'production'
 after "deploy:stop",    "delayed_job:stop"
 after "deploy:start",   "delayed_job:start"
 after "deploy:restart", "delayed_job:restart"
@@ -37,6 +37,23 @@ namespace :deploy do
     end
   end
   
+  namespace :delayed_job do
+
+  desc "Stop the delayed_job process"
+  task :stop, :roles => :app do
+    run "cd #{current_path}; script/delayed_job -e #{rails_env} stop"
+  end
+
+  desc "Start the delayed_job process"
+  task :start, :roles => :app do
+    run "cd #{current_path}; script/delayed_job -e #{rails_env} start"
+  end
+
+  desc "Restart the delayed_job process"
+  task :restart, :roles => :app do
+    run "cd #{current_path}; script/delayed_job -e #{rails_env} restart"
+  end
+end
 
   task :setup_config, roles: :app do
     sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
