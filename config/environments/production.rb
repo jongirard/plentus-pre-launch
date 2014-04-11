3155d98a3815cd01efaffdfe7cca4650b1f94854
Jon Girard <jongirard03@gmail.com>
2014-04-11 12:28:32 -0700

    remove old ruby version, update production assets compiler and nginx conf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,12 +56,12 @@ PlentusPreLaunch::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
-  config.asset_host = 'goplentus.com'
+  config.asset_host = 'https://goplentus.com'
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
-
+  config.assets.precompile += %w( authorized_application.css authorized_application.js )
+  
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
